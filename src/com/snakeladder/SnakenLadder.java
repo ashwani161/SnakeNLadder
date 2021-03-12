ae5f641a9db7b30a0c78f1f5b7509c976c53840b
ashwani161 <ashwanin65@gmail.com>
2021-03-12 19:07:58 +0530

    Added:random rolldice

--- src/com/snakeladder/SnakenLadder.java
+++ src/com/snakeladder/SnakenLadder.java
@@ -4,6 +4,9 @@ public class SnakenLadder {
 	static void myMethod() {
 		int intposition = 0;
 		System.out.println("Player at startposition: " + intposition);
+		int rolldice = 1+ (int)( (Math.random() *10) % 6);
+		System.out.println("Player rolled dice: " +rolldice);
+		
 	}
 	public static void main(String[] args) {
 		myMethod();
