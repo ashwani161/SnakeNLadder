98ae973edb4cb709aa218332bdb447fcbd3124ee
ashwani161 <ashwanin65@gmail.com>
2021-03-13 07:14:08 +0530

    Added:rolldice reached till 100 and number of throws make

--- src/com/snakeladder/SnakenLadder.java
+++ src/com/snakeladder/SnakenLadder.java
@@ -22,7 +22,26 @@ public class SnakenLadder {
 
 	}
 
+	static void myMethod2() {
+		int N = 0;
+		int count = 0;
+		do {
+			count++;
+			int rolldice = 1 + (int) ((Math.random() * 10) % 6);
+			if (N + rolldice <= 100) {
+				N = N + rolldice;
+			} else {
+				System.out.println("Could not move coin, you should get " + (100 - N) + " to win");
+				continue;
+			}
+			System.out.println("Coin moved to " + N);
+		} while (N != 100);
+		System.out.println("Player has reached 100 after " + count + " throws");
+		System.out.println("You won after " + count + " dice throws");
+	}
+
 	public static void main(String[] args) {
 		myMethod();
+		myMethod2();
 	}
 }
